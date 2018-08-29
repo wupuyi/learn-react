75b4af617c844694f6a5d83764cb4a9775e05099
wupuyi <305652984@qq.com>
2018-08-29 16:24:28 +0800

    组件的state和setState

--- react-test3/hello-react/src/index.js
+++ react-test3/hello-react/src/index.js
@@ -7,46 +7,23 @@ import './index.css';
 // ReactDOM.render(<App />, document.getElementById('root'));
 // registerServiceWorker();
 
-class Title extends Component {
-  handleClickOnTitle (word, e) {
-    console.log('Click on title.')
-    console.log(e.target.innerHTML)
-    console.log(this, word)
+class LikeButton extends Component {
+  constructor () {
+    super ()
+    this.state = { isLiked: false }
   }
-  render () {
-    return (
-      <h1 onClick={this.handleClickOnTitle.bind(this, 'Hello')}>React 小书</h1>
-    )
-  }
-}
 
-class Header extends Component {
-  render () {
-    return (
-      <div>
-        <Title />
-        <h2>This is Header</h2>
-      </div>
-    )
+  handleClickOnLikeButton () {
+    this.setState({
+      isLiked: !this.state.isLiked
+    })
   }
-}
 
-class Main extends Component {
   render () {
     return (
-      <div>
-        <h2>This is main content</h2>
-      </div>
-    )
-  }
-}
-
-class Footer extends Component {
-  render () {
-    return (
-      <div>
-        <h2>This is footer</h2>
-      </div>
+      <button onClick={this.handleClickOnLikeButton.bind(this)}>
+        {this.state.isLiked ? '取消' : '点赞'}👍
+      </button>
     )
   }
 }
@@ -55,9 +32,7 @@ class Index extends Component {
   render () {
     return (
       <div>
-        <Header />
-        <Main />
-        <Footer />
+        <LikeButton />
       </div>
     )
   }
