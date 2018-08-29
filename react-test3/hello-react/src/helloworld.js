6d2d166babcf86933e4c81ac843b2830f0c95f46
wupuyi <305652984@qq.com>
2018-08-29 18:11:27 +0800

    渲染列表数据

---
+++ react-test3/hello-react/src/helloworld.js
@@ -0,0 +1,24 @@
+// 通过集成Component构建
+class HelloWorld extends Component {
+  constructor() {
+    super()
+  }
+
+  sayHi () {
+    alert('Hello World')
+  }
+
+  render () {
+    return (
+      <div onClick={this.sayHi.bind(this)}>Hello World</div>
+    )
+  }
+}
+
+// 通过函数式
+const HelloWorld = (props) => {
+  const sayHi = (event) => alert('Hello World')
+  return (
+    <div onClick={sayHi}>Hello World</div>
+  )
+}
--- react-test3/hello-react/src/index.js
+++ react-test3/hello-react/src/index.js
@@ -33,6 +33,13 @@ class LikeButton extends Component {
   }
 }
 
+const users = [
+  { username: 'Jerry', age: 21, gender: 'male' },
+  { username: 'Tomy', age: 22, gender: 'male' },
+  { username: 'Lily', age: 19, gender: 'female' },
+  { username: 'Lucy', age: 20, gender: 'female' }
+]
+
 class Index extends Component {
   constructor () {
     super()
@@ -50,6 +57,17 @@ class Index extends Component {
   }
 
   render () {
+    const userElements = [] // 保存每个用户渲染以后JSX的数组
+    for (let user of users) {
+      userElements.push( // 循环每个用户，构建JSX，push到数组中
+        <div>
+          <div>姓名: {user.username}</div>
+          <div>年龄: {user.age}</div>
+          <div>性别: {user.gender}</div>
+          <hr />
+        </div>
+      )
+    }
     return (
       <div>
         <LikeButton 
@@ -60,6 +78,7 @@ class Index extends Component {
             修改 wordings
           </button>
         </div>
+        <div>{userElements}</div>
       </div>
     )
   }
