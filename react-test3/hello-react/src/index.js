980d98398cf4d805b95ae03988ac330cc02800b5
wupuyi <305652984@qq.com>
2018-08-29 15:09:07 +0800

    了解jsx

--- react-test3/hello-react/src/index.js
+++ react-test3/hello-react/src/index.js
@@ -1,8 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
+
+class Title extends Component {
+  render () {
+    return (
+      <h1>React 小书</h1>
+    )
+  }
+}
+
+class Header extends Component {
+
+  renderGoodWord (goodWord, badWord) {
+    const isGoodWord = true
+    return isGoodWord ? goodWord : badWord
+  }
+
+  render () {
+    return (
+      <div>
+        <h1>
+          React 小书 
+          {this.renderGoodWord(
+            <strong> is good </strong>,
+            <span> is not good </span>
+          )
+          }
+        </h1>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <Header />,
+  document.getElementById('root')
+)
