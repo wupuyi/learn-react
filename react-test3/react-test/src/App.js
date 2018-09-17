bd6dfcfb9f5f528c173413362d2a2a7b4cd43d07
wupuyi <305652984@qq.com>
2018-09-17 14:45:40 +0800

    ceshi

---
+++ react-test3/react-test/src/App.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+class MyApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      inputValue: event.target.value
+    });
+  }
+  render() {
+    return (
+      <div>
+        { /* change code below this line */}
+        <GetInput
+          value={this.state.inputValue}
+          handleChange={this.state.handleChange} />
+        <RenderInput input={this.state.inputValue} />
+        { /* change code above this line */}
+      </div>
+    );
+  }
+};
+
+class GetInput extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h3>Get Input:</h3>
+        <input
+          value={this.props.input}
+          onChange={this.props.handleChange} />
+      </div>
+    );
+  }
+};
+
+class RenderInput extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h3>Input Render:</h3>
+        <p>{this.props.input}</p>
+      </div>
+    );
+  }
+};
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <MyApp />
+      </div>
+    );
+  }
+}
+
+export default App;
