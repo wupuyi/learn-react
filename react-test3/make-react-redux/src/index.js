850852f5befdeaef659b710379aa64b44410958b
wupuyi <305652984@qq.com>
2018-12-12 17:44:30 +0800

    commit

--- react-test3/make-react-redux/src/index.js
+++ react-test3/make-react-redux/src/index.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
+import Header from './Header';
+import Content from './Content';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Index extends Component {
+  render () {
+    return (
+      <div>
+        <Header />
+        <Content />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<Index />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
