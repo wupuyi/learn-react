e3aca2f987df5cb67805644969d2ed30af50e778
wupuyi <305652984@qq.com>
2018-12-13 17:47:48 +0800

    tijiao

--- react-test3/make-react-redux/src/Content.js
+++ react-test3/make-react-redux/src/Content.js
@@ -3,10 +3,36 @@ import PropTypes from 'prop-types';
 import ThemeSwitch from './ThemeSwitch';
 
 class Content extends Component {
+  static contextTypes = {
+    store: PropTypes.object
+  }
+
+  constructor() {
+    super();
+    this.state = {
+      themeColor: ''
+    }
+  }
+  
+
+  componentWillMount () {
+    this._updateThemeColor()
+  }
+
+  _updateThemeColor () {
+    const { store } = this.context;
+    const state = store.getState();
+    console.log(state)
+    this.setState({
+      themeColor: state.themeColor
+    });
+    console.log(this.state.themeColor)
+  }
+
   render () {
     return (
       <div>
-        <p>React.js 小书内容</p>
+        <p style={{ color: this.state.themeColor }}>React.js 小书内容</p>
         <ThemeSwitch />
       </div>
     )
